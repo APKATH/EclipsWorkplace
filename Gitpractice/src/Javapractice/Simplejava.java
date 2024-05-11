ef32c690f4c38c824e97ef64aed6ed9c6f168233
APKATH <aparnathakare20@gmail.com>
2024-05-11 18:21:09 +0530

    Firt Commit

---
+++ Gitpractice/src/Javapractice/Simplejava.java
@@ -0,0 +1,20 @@
+package Javapractice;
+
+import java.util.Arrays;
+
+public class Simplejava {
+	public static void main(String[] args) {
+		int[] arr= {24,68,79,56,19,87,67,24,35};
+		Arrays.sort(arr);
+		for(int i=0;i<=arr.length-1;i++)
+		{
+		System.out.println(arr[i]);
+		}System.out.println("2nd Largest Number is:"+ arr);
+		}
+		}
+
+
+
+	
+
+
